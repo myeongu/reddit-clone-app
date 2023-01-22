0ffdca2ce639e1148de4c326326e3ae6c0ae5213
myeongu <ausrndi@gmail.com>
2023-01-22 22:50:40 +0900

    [FEAT] Sign up and login

---
+++ client/src/pages/register.tsx
@@ -0,0 +1,71 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router';
+import React, { FormEvent, useState } from 'react'
+import InputGroup from '../components/InputGroup'
+import axios from 'axios';
+
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState<any>({});
+
+  const router = useRouter();
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    try {
+      const res = await axios.post("/auth/register", {
+        email,
+        password,
+        username,
+      });
+      console.log('res', res);
+      router.push("/login");
+    } catch (error: any) {
+      console.log('error', error);
+      setErrors(error.response.data || {});
+    }
+  }
+
+  return (
+    <div className='bg-white'>
+        <div className='flex flex-col items-center justify-center h-screen p-6'>
+            <div className='w-10/12 mx-auto md:w-96'>
+                <h1 className='mb-2 text-lg font-mediu'>회원가입</h1>
+                <form onSubmit={handleSubmit}>
+                  <InputGroup 
+                    placeholder='Email'
+                    value={email}
+                    setValue={setEmail}
+                    error={errors.email}
+                  />
+                  <InputGroup 
+                    placeholder='Username'
+                    value={username}
+                    setValue={setUsername}
+                    error={errors.username}
+                  />
+                  <InputGroup 
+                    placeholder='Password'
+                    value={password}
+                    setValue={setPassword}
+                    error={errors.password}
+                  />
+                  <button className='w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded'>
+                    Sign Up
+                  </button>
+                </form>
+                <small>
+                  이미 가입하셨나요?
+                  <Link href="/login" className='ml-1 text-blue-500 uppercase'>
+                    로그인
+                  </Link>
+                </small>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Register
